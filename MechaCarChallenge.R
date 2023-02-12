0d8cfc04d312cac160703498b81bb08448b5544f
Kyle <kyle.brumbaugh9@gmail.com>
2023-02-12 16:22:47 -0500

    Pushing Deliverable 2

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -4,3 +4,9 @@ library(tidyverse)
 mechaCar <- read_csv("MechaCar_mpg.csv")
 
 summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,mechaCar))
+
+suspensionCoil <- read_csv("Suspension_Coil.csv")
+
+total_summary <- suspensionCoil %>% summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI))
+
+lot_summary <- suspensionCoil %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI))
